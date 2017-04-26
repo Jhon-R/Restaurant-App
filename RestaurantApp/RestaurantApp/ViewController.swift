4e6b6ca1f18d753071c1acb47fcfa652516aa6ce
JONATHAN OSWALDO RUIZ <jruiz@10-148-141-140.dhcp-m.deakin.edu.au>
2017-04-27 02:31:11 +1000

    first

---
+++ RestaurantApp/RestaurantApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by JONATHAN OSWALDO RUIZ on 27/04/2017.
+//  Copyright © 2017 JONATHAN OSWALDO RUIZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
